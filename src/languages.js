c2cdca8de7259d04f06734a1f58ebff874c554b6
gabe <16624137+doge@users.noreply.github.com>
2023-05-01 10:40:04 -0600

    Implementing catalan translation

---
+++ src/languages.js
@@ -0,0 +1,14 @@
+const languages = {
+    en: {
+        translation: {
+            "about_description": "I'm Gabe, a passionate programmer who has been coding for around 5 years now. I am experienced in web development with a strong focus on the back-end. Currently though, I've been loving working with React. Feel free to view some of my open source projects.",
+            "about_language_header": "Languages I've been working with recently",
+        },
+    },
+    cat: {
+        translation: {
+            "about_description": "Sóc en Gabe, un programador apassionat que fa uns 5 anys que està programant. Tinc experiència en desenvolupament web amb un fort enfocament al back-end. Actualment, però, m'ha agradat molt treballar amb React. No dubteu a veure alguns dels meus projectes de codi obert.",
+            "about_language_header": "Idiomes amb els que he treballat recentment"
+        },
+    }
+}
